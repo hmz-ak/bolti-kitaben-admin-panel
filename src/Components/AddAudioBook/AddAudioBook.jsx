a6906f4d3e00e3f2c80245f903b361c70638c276
imhamza1 <hamza.akram4635@gmail.com>
2021-09-15 11:33:18 +0500

    feat: isMobile implemented

--- src/Components/AddAudioBook/AddAudioBook.jsx
+++ src/Components/AddAudioBook/AddAudioBook.jsx
@@ -1,15 +1,63 @@
-import { Grid } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
+import { isMobile } from "react-device-detect";
+import { Container, Grid, TextField } from "@material-ui/core";
 
 const AddAudioBook = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [type, setType] = useState("");
+  const [description, setDescription] = useState("");
   return (
-    <div>
-      <Grid align="center" container>
+    <Container maxWidth="md">
+      <Grid container>
         <Grid item xs={12}>
-          Hey
+          <Grid align={isMobile ? "center" : "left"} container>
+            <Grid item xs={12} lg={4}>
+              <TextField
+                style={isMobile ? { width: "100%" } : {}}
+                required
+                id="standard-required"
+                label="Book Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} lg={4}>
+              <TextField
+                style={isMobile ? { width: "100%" } : {}}
+                required
+                id="standard-required"
+                label="Author Name"
+                onChange={(e) => setAuthor(e.target.value)}
+                value={author}
+              />
+            </Grid>
+            <Grid item xs={12} lg={4}>
+              <TextField
+                style={isMobile ? { width: "100%" } : {}}
+                required
+                id="standard-required"
+                label="Book Type"
+                value={type}
+                onChange={(e) => setType(e.target.value)}
+              />
+            </Grid>
+            <Grid xs={12} item>
+              <TextField
+                style={{ width: "100%", marginTop: 40 }}
+                id="outlined-multiline-static"
+                label="Enter Book Description"
+                multiline
+                rows={10}
+                onChange={(e) => setDescription(e.target.value)}
+                value={description}
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
-    </div>
+    </Container>
   );
 };
 
