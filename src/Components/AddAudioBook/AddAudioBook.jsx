bcf76a874150f76324cdb10a411e6f4ace9873fa
imhamza1 <hamza.akram4635@gmail.com>
2021-09-20 01:29:32 +0500

    imageInput component implemened and multiple select categories implemented in audioBooks component

--- src/Components/AddAudioBook/AddAudioBook.jsx
+++ src/Components/AddAudioBook/AddAudioBook.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import { isMobile } from "react-device-detect";
 import { Container, Grid, TextField } from "@material-ui/core";
-
+import CategorySelect from "./CategorySelect";
+import ImageInput from "../ImageInput/ImageInput"
 const AddAudioBook = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [type, setType] = useState("");
   const [description, setDescription] = useState("");
+  const [img, setImg] = useState(null);
+
   return (
     <Container maxWidth="md">
       <Grid container>
@@ -33,15 +36,9 @@ const AddAudioBook = () => {
               />
             </Grid>
             <Grid item xs={12} lg={4}>
-              <TextField
-                style={isMobile ? { width: "100%" } : {}}
-                required
-                id="standard-required"
-                label="Book Type"
-                value={type}
-                onChange={(e) => setType(e.target.value)}
-              />
+              <CategorySelect />
             </Grid>
+
             <Grid xs={12} item>
               <TextField
                 style={{ width: "100%", marginTop: 40 }}
@@ -52,6 +49,16 @@ const AddAudioBook = () => {
                 onChange={(e) => setDescription(e.target.value)}
                 value={description}
                 variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12}>
+               <ImageInput
+                nameAttr={"BookImage"}
+                idAttr={"book-img"}
+                uploadText={"jpeg/jpg/png/gif"}
+                getter={img}
+                setter={(e) => setImg(e.target.files[0])}
+                helperText={", size limit: 40 mb"}
               />
             </Grid>
           </Grid>
---
+++ src/Components/AddAudioBook/CategorySelect.jsx
@@ -0,0 +1,114 @@
+import React,{useState} from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import ListItemText from "@material-ui/core/ListItemText";
+import Select from "@material-ui/core/Select";
+import Checkbox from "@material-ui/core/Checkbox";
+import Chip from "@material-ui/core/Chip";
+import { useEffect } from "react";
+import categoryService from "../services/CategoryService";
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    // margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 300,
+  },
+  chips: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  chip: {
+    margin: 2,
+  },
+  noLabel: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+export default function MultipleSelect() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [categoryData,setCategoryData]=useState([]);
+  const [personName, setPersonName] = React.useState([]);
+  useEffect(() => {
+    categoryService.getCategory()
+      .then((data) => {
+        setCategoryData(data)
+        console.log(data)
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  const handleChange = (event) => {
+    setPersonName(event.target.value);
+  };
+
+  // const handleChangeMultiple = (event) => {
+  //   const { options } = event.target;
+  //   const value = [];
+  //   for (let i = 0, l = options.length; i < l; i += 1) {
+  //     if (options[i].selected) {
+  //       value.push(options[i].value);
+  //     }
+  //   }
+  //   setPersonName(value);
+  // };
+
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-mutiple-chip-label">Categories</InputLabel>
+        <Select
+          labelId="demo-mutiple-chip-label"
+          id="demo-mutiple-chip"
+          multiple
+          value={personName}
+          onChange={handleChange}
+          input={<Input id="select-multiple-chip" />}
+          renderValue={(selected) => (
+            <div className={classes.chips}>
+              {selected.map((value) => (
+                <Chip key={value} label={value} className={classes.chip} />
+              ))}
+            </div>
+          )}
+          MenuProps={MenuProps}
+        >
+          {categoryData?.map((name,index) => (
+            <MenuItem
+              key={index}
+              value={name.name}
+              style={getStyles(name, personName, theme)}
+            >
+              {name.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
